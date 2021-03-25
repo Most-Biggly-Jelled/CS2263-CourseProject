d9c035c90b9f4b10eafdbb24a071222a12fd680d
madz23 <maymadi@isu.edu>
2021-03-25 12:37:54 -0600

    Task and Subtask 'completed'

--- app/src/main/java/CS2263/CourseProject/Subtask.java
+++ app/src/main/java/CS2263/CourseProject/Subtask.java
@@ -1,4 +1,17 @@
 package CS2263.CourseProject;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
 public class Subtask {
+    @Getter @Setter private String priority;
+    @Getter @Setter private String title;
+    @Getter @Setter private int date;
+    @Getter @Setter private String description;
+    @Getter @Setter private String status;
+    @Getter @Setter private List<String> labels;
+   // @Getter @Setter private TaskList parentList;
+
 }
